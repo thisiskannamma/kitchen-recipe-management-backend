94b1dd52725de4bc32f356cf08fd1e9537907117
thisiskannamma <MY_NAME@example.com>
2024-02-05 12:22:23 +0530

    yes

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -22,3 +22,21 @@ app.use('/category',categoryRoutes)
 const PORT=process.env.PORT || 3000;
 
 app.listen(PORT,()=>{console.log(`server conneted to port ${PORT}`)});
+
+app.get('/', (req, res) => res.send(`
+<div>
+<p>Because my rendor app couldn't able to connect to mongoDB compass.please use this link to check details  </p>
+<p>To get all mentor List - <a>http://localhost:5000/Mentors<a> </p>
+<br>
+<p>To get all Students List - http://localhost:5000/Students </p>
+<br>
+<p>To get mentor based on ID - http://localhost:5000/Mentors/get-mentor/65a4d070b0657ec3378751c5<p>
+<br>
+<p>To get students with no mentor - http://localhost:5000/Students/no-mentors<p>
+<br>
+<p>To get all students for a particular mentor - http://localhost:5000/Students/mentor-students/65a4ce5db0657ec3378751c0 </p>
+<br>
+
+
+
+`))
